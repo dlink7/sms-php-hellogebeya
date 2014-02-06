33afc919cb19537b6cbc22e1b864ae9f8a870c46
Leul <leulteferi735@gmail.com>
2014-02-06 16:04:03 +0300

    used header()

--- index.php
+++ index.php
@@ -5,22 +5,24 @@
 //print_r($_GET);
 $key1 = "xxx";
 $key2 = "yyy";
-$more_info = "<br>For more information call 8421";
+$more_info = "\nFor more information call 8421";
 
-$sms1_confirmation_code = "Your code is $key1 please enter it in the web site";
-$sms2_confirmation_congra = "Congratulation you have been register on HelloGebeya you can now post your profile or adds on hellogebeya.com or call 8421.";
-$sms3_confirmation_inform = " As a member of hellogebeya,com we would also like to inform you that you can fully benefit from our other helloservices like HelloDoctor Call 8896 in case you need Medical Advise.";
+$sms1_confirmation_code = "Your code is $key1. Please enter it on the website";
+$sms2_confirmation_congra = "Congratulations! You have been registered on HelloGebeya. You can now post ads on hellogebeya.com";
+$sms3_confirmation_inform = "Please check out our other HelloService: 'HelloDoctor'. Call 8896 for 24/7 Medical Advice, Home Visit and Ambulance Transportation";
 $sms4_point_transfer_sender = "You have sent $key1 points to $key2";
-$sms5_point_transfer_reciever = "You have gotten $key1 points from $key2";
-$sms6_publish_add = "Congratulation you have published the following Add item  $key1 Title : $key2";
-$sms7_unpublish_add = "We confirm you that you have unpublished the following Add item $key1 Title : $key2";
-$sms8_modify_add = "We confirm you that you have modified the following add item $key1 Title : $key2";
-$sms9_disable_account = "We inform you that your account have been disabled for none conformity with the terms and conditions of HelloGabeya";
-$sms10_enable_account = "We are happy to inform you that your account has been re-anabled";
-$sms11_disable_add = "We inform you that your add item $key1, title $key2 have been deactivated for none conformity with our term and conditions";
-$sms12_enable_add = "We inform you that your add item $key1, title $key2 have been reactivated";
+$sms5_point_transfer_reciever = "You have received $key1 points from $key2";
+$sms6_publish_add = "Congratulations! You have published the following Ad item  $key1 Title: $key2";
+$sms7_unpublish_add = "The following Ad item $key1 Title: $key2 has been UNPUBLISHED";
+$sms8_modify_add = "The following Ad item $key1 Title: $key2 has been MODIFIED";
+$sms9_disable_account = "Your Account has been DISABLED for non-conformity with the terms and conditions of HelloGebeya";
+$sms10_enable_account = "We are happy to inform you that your account has been RE-ENABLED";
+$sms11_disable_add = "Your Ad item $key1 Title: $key2 has been DEACTIVATED non-conformity with the terms and conditions of HelloGebeya";
+$sms12_enable_add = "We are happy to inform you that your Ad item $key1 Title: $key2 has been RE-ENABLED";
+$sms13_point_transfer_receiver_2 = "You have received $key1 points from $key2. To get your points, please create an account on HelloGebeya.com";
+$sms14_account_created = "Your account has been created. Your password is $key1";
 
-$help = "Usage :  index.php?<b>p</b>=PHONE_NUMBER&<b>s</b>=CASE_NUMBER(1-12)<br><br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;For case 1 :  Additional Parameter <b>a</b><br>For Cases 4-8 and 11-12 :  Additional Parameters <b>a</b> and <b>b</b><br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;For the rest Cases :  No Additional Parameters";
+$help = "Usage :  index.php?<b>p</b>=PHONE_NUMBER&<b>s</b>=CASE_NUMBER(1-14)<br><br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;For case 1 and 14 :  Additional Parameter <b>a</b><br>For Cases 4-8 and 11-13 :  Additional Parameters <b>a</b> and <b>b</b><br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;For the rest Cases :  No Additional Parameters";
 
 $arr  = array(
 	'1' => $sms1_confirmation_code.$more_info,
@@ -35,6 +37,8 @@ $arr  = array(
 	'10' => $sms10_enable_account.$more_info,
 	'11' => $sms11_disable_add.$more_info,
 	'12' => $sms12_enable_add.$more_info,
+	'13' => $sms13_point_transfer_receiver_2,
+	'14' => $sms14_account_created
 	);
 
 $p = $_GET['p'];
@@ -52,9 +56,9 @@ echo("Empty Parameter: <b>p</b>,<b>s</b><br><br><br>".$help);
 
 	if ($s=="2" || $s=="3" || $s=="9" || $s=="10") {
 	        $sms_txt = $arr[$s];
-	}elseif($s == "1" && !empty($a)){
+	}elseif(($s == "1" || $s == "14" ) && !empty($a)){
 			$sms_txt = str_replace($key1, $a, $arr[$s]);
-	}elseif(  ($s == "4" ||$s == "5" ||$s == "6" ||$s == "7"||$s == "8"||$s == "11"||$s == "12") &&  !empty($a) && !empty($b)){
+	}elseif(  ($s == "4" ||$s == "5" ||$s == "6" ||$s == "7"||$s == "8"||$s == "11"||$s == "12"||$s == "13") &&  !empty($a) && !empty($b)){
 			$sms_txt_temp = str_replace($key1, $a, $arr[$s]);
 			$sms_txt = str_replace($key2, $b, $sms_txt_temp);
 	}else{
@@ -67,7 +71,20 @@ echo("Empty Parameter: <b>p</b>,<b>s</b><br><br><br>".$help);
 }
 
 //count($_GET)==2
-
+if (!function_exists('http_response_code'))
+{
+    function http_response_code($newcode = NULL)
+    {
+        static $code = 200;
+        if($newcode !== NULL)
+        {
+            header('X-PHP-Response-Code: '.$newcode, true, $newcode);
+            if(!headers_sent())
+                $code = $newcode;
+        }       
+        return $code;
+    }
+}
 
 function send_sms($phone_number,$sms_text)
 {
@@ -91,15 +108,15 @@ if($curl_return_code == "202"){
 
 if($resp == "0: Accepted for delivery"){
 echo("200 (SMS Requested)<br>");
-http_response_code(200);
+header("HTTP/1.1 200 OK");
 }else{
 echo("202 (ETC is Unreachable)<br>");
-http_response_code(202);
+header("HTTP/1.1 202 Unreachable");
 }
 
 }elseif($curl_return_code == "0"){
 echo("503 (SMS Gateway is Unavailable)<br>");
-http_response_code(503);
+header("HTTP/1.1 503 Unavailable");
 }else {
 echo($curl_return_code." ".$resp." (new case)<br>");
 }
